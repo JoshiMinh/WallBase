cb1e7b6d39462a1c5461c12d6bb4239703f75d90
Joshi Minh <binhangia241273@gmail.com>
2025-09-25 11:29:00 +0700

    Wallbase App Error

--- app/src/main/java/com/joshiminh/wallbase/WallBaseApp.kt
+++ app/src/main/java/com/joshiminh/wallbase/WallBaseApp.kt
@@ -5,18 +5,17 @@ import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.disk.DiskCache
 import com.joshiminh.wallbase.util.network.ServiceLocator
-import java.io.File
-import okio.Path.Companion.toPath
+import okio.Path.Companion.toOkioPath
 
 class WallBaseApp : Application() {
     override fun onCreate() {
         super.onCreate()
         ServiceLocator.initialize(this)
-        SingletonImageLoader.set { context ->
+        SingletonImageLoader.imageLoader = { context ->
             ImageLoader.Builder(context)
                 .diskCache {
                     DiskCache.Builder()
-                        .directory(File(context.cacheDir, "coil_previews").absolutePath.toPath())
+                        .directory(context.cacheDir.resolve("coil_previews").toOkioPath())
                         .maxSizeBytes(50L * 1024 * 1024)
                         .build()
                 }
